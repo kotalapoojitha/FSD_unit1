d0093edab08b689cc0a680a55ed0179a286e6c80
kotalapoojitha <kotalapoojitha@gmail.com>
2025-07-08 21:16:05 +0530

    Create arrowfn.js

---
+++ arrowfn.js
@@ -0,0 +1,9 @@
+// Traditional function
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+// Arrow function equivalent
+const greetArrow = (name) => `Hello, ${name}!`;
+// Usage
+console.log(greet("Farheen")); // Output: Hello, Farheen!
+console.log(greetArrow("Farheen")); // Output: Hello, Farheen!
