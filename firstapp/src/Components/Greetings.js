74d06f70c56fd486b9ca39cf6e0b4fd2b85da561
kotalapoojitha <kotalapoojitha@gmail.com>
2025-07-14 18:20:50 +0530

    Create Greetings.js

---
+++ firstapp/src/Components/Greetings.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+export default class Greetings extends Component {
+    render() {
+        return(
+            <div>
+                <p> Welcome to React World !</p>
+                <h1> Good Morning I am your first Component</h1>
+            </div>
+        )
+    }
+}
