f3623278ddd973b5f2ab7658557ab3a2434d7b9d
kotalapoojitha <kotalapoojitha@gmail.com>
2025-07-14 18:20:54 +0530

    Create Hobbies.js

---
+++ firstapp/src/Components/Hobbies.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Hobbies extends Component {
+    render() {
+        return(
+            <div>
+                <h3> My hobbies are Reels,movies,music...n times as n tends to infinity</h3>
+            </div>
+        )
+    }
+
+}
