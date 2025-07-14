a96cacafa57ef4977dbf545369ba33974efd8ad9
kotalapoojitha <kotalapoojitha@gmail.com>
2025-07-14 18:21:02 +0530

    Create Qualifications.js

---
+++ firstapp/src/Components/Qualifications.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Qualifications extends Component {
+    render() {
+        return(
+            <div>
+                <h3> I did Msc(Applied Mathematices),Msc (Computer Science),M.Tech (Software Engineer) </h3>
+            </div>
+        )
+    }
+}
