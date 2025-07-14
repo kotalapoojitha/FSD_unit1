8318e3f44e7dda18e96825943b112ef11f8d0dd4
kotalapoojitha <kotalapoojitha@gmail.com>
2025-07-14 18:20:58 +0530

    Create Introduction.js

---
+++ firstapp/src/Components/Introduction.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import App from "../App";
+export default class Introduction extends Component {
+    render() {
+        return(
+            <div>
+                <h1> Welcome to React World </h1>
+                <p> This is your first component </p>
+            </div>
+        )
+    }
+}
