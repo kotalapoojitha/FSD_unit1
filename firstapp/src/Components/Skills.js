01e6a2621e98d9a9e629ddd1bc5be6683c2d42c8
kotalapoojitha <kotalapoojitha@gmail.com>
2025-07-17 09:38:45 +0530

    Create Skills.js

---
+++ firstapp/src/Components/Skills.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div>
+      <hi> Hi my skills are python, ML and WebDevelopment</hi>
+    </div>
+  )
+}
+export default Skills
